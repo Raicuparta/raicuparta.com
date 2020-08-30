29f97ff7f56fe391650a601f30c6bffc9b5382d7
Ricardo Lopes <raicuparta@gmail.com>
2020-08-30 03:14:16 +0200

    Refactor project card

---
+++ components/project-card.tsx
@@ -0,0 +1,25 @@
+import { Project } from '../types';
+import styles from './project-card.module.scss';
+import { Icon } from '.';
+
+type Props = {
+  project: Project;
+};
+
+export const ProjectCard = ({ project }: Props) => (
+  <a key={project.title} className={styles.projectCard} href={project.url}>
+    <h3 className={styles.title}>{project.title}</h3>
+    <p className={styles.description}>{project.description}</p>
+    <div>
+      {project.tags.map((tag) => (
+        <span className={styles.tag}>{tag}</span>
+      ))}
+    </div>
+    {project.stars > 0 && (
+      <span>
+        <Icon className={styles.starIcon} name="Star" />
+        {project.stars}
+      </span>
+    )}
+  </a>
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,60 @@
 import css from 'styled-jsx/css';
 
-import { Face, Header, SocialLinks, Projects } from '../components';
+import { Project } from '../types';
+import { Header, SocialLinks, ProjectsSection } from '../components';
 import styles from './index.module.scss';
+
+const projects: Project[] = [
+  {
+    title: 'NomaiVR',
+    description:
+      'Modification for the game "Outer Wilds" that converts it from a regular "flat" 3D game to a full VR experience',
+    url: 'https://github.com/Raicuparta/nomai-vr',
+    tags: ['C#', '.NET', 'Unity'],
+    stars: 0,
+  },
+  {
+    title: 'Outer Wilds Mod Manager',
+    description:
+      'Windows app for installing and downloading game modifications.',
+    url: 'https://github.com/Raicuparta/ow-mod-manager',
+    tags: ['Electron', 'TypeScript', 'React'],
+    stars: 0,
+  },
+  {
+    title: 'Outer Wilds Mods Website',
+    description: 'Web page with information about game modifications.',
+    url: 'https://github.com/Raicuparta/outerwildsmods.com',
+    tags: ['TypeScript', 'Next.js', 'React'],
+    stars: 0,
+  },
+  {
+    title: 'Curvatron',
+    description:
+      'Minimalist "snake" video game, available on Steam. Previously available on Android and iOS.',
+    url: 'https://github.com/bravebunny/curvatron',
+    tags: ['JavaScript', 'Phaser.JS'],
+    stars: 0,
+  },
+];
+
+const articles: Project[] = [
+  {
+    title:
+      'Dealing with Platform Inconsistencies as a North Korean Front-End Developer',
+    url:
+      'https://dev.to/raicuparta/dealing-with-platform-inconsistencies-as-a-north-korean-front-end-developer-3158',
+    tags: ['HTML', 'JavaScript', 'CSS'],
+    stars: 0,
+  },
+  {
+    title: 'Ditching worthless friends with Facebook data and JavaScript',
+    url:
+      'https://dev.to/raicuparta/ditching-worthless-friends-with-facebook-data-and-javascript-3f2i',
+    tags: ['JavaScript'],
+    stars: 0,
+  },
+];
 
 const Home = () => (
   <>
@@ -9,7 +62,8 @@ const Home = () => (
     <main className={styles.main}>
       <div className={styles.pageBody}>
         <Header title="Ricardo Lopes" subtitle="Front-end Developer" />
-        <Projects />
+        <ProjectsSection title="Popular Projects" projects={projects} />
+        <ProjectsSection title="Popular Articles" projects={articles} />
       </div>
       <SocialLinks />
     </main>
