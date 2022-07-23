bc727888c86fd8fce336a80b2009e421ff0e36ac
Raicuparta <Raicuparta@users.noreply.github.com>
2022-07-23 02:56:01 +0200

    Add page for each mod

---
+++ pages/[modId].tsx
@@ -0,0 +1,37 @@
+import { Header, SocialLinks, ProjectsSection, Link } from '../components';
+import { ModCard } from '../components/mod-card';
+import styles from './styles/index.module.scss';
+import mods from './mods.json';
+import { useRouter } from 'next/router';
+
+const ModPage = () => {
+  const router = useRouter();
+  const { modId } = router.query;
+  const mod = mods.find(({ gameKey }) => gameKey === modId);
+
+  return (
+    <>
+      <title>Raicuparta</title>
+      <main className={styles.main}>
+        <div className={styles.pageBody}>
+          <Header title="Raicuparta" />
+          <SocialLinks />
+          <div className={styles.content}>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '2em',
+                width: '100%',
+              }}
+            >
+              {mod && <ModCard {...mod} />}
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default ModPage;
