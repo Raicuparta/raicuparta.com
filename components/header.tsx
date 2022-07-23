3304eb999ee7d615f83023779207d67efca3a24e
Raicuparta <Raicuparta@users.noreply.github.com>
2022-07-23 03:10:26 +0200

    Tailwind header

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,17 @@
-import styles from './styles/header.module.scss';
-
 type Props = {
   title: string;
 };
 
 export const Header = ({ title }: Props) => (
-  <header className={styles.wrapper}>
-    <div className={styles.titleWrapper}>
-      <img height="60" src="img/wario.png" className={styles.avatar} />
-      <h1 className={styles.title}>{title}</h1>
+  <header className="bg-black bg-opacity-20 p-4">
+    <div className="flex gap-4 justify-center items-center">
+      <img
+        height="60"
+        width="60"
+        src="img/wario.png"
+        className="rounded-full"
+      />
+      <h1 className="text-4xl">{title}</h1>
     </div>
   </header>
 );
