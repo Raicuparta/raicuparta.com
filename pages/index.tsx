ebc88a5296dff2723ab36062ca2cb842cb5461ed
Raicuparta <Raicuparta@users.noreply.github.com>
2022-07-23 03:33:47 +0200

    index cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,17 @@
 import { Header, SocialLinks } from '../components';
 import { ModCard } from '../components/mod-card';
-import styles from './styles/index.module.scss';
 import mods from './mods.json';
 
 const Home = () => (
   <>
     <title>Raicuparta</title>
-    <main className={styles.main}>
-      <div className={styles.pageBody}>
-        <Header title="Raicuparta" />
-        <SocialLinks />
-        <div className={styles.content}>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '2em',
-              width: '100%',
-            }}
-          >
-            {mods.map((mod) => (
-              <ModCard key={mod.gameKey} {...mod} />
-            ))}
-          </div>
-        </div>
+    <main>
+      <Header title="Raicuparta" />
+      <SocialLinks />
+      <div className="max-w-3xl m-auto p-4 flex flex-col gap-4">
+        {mods.map((mod) => (
+          <ModCard key={mod.gameKey} {...mod} />
+        ))}
       </div>
     </main>
   </>
