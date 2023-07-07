0efdb28d615fc5cf74d5cd4478f6e994142fbdeb
Raicuparta <Raicuparta@users.noreply.github.com>
2023-07-07 14:07:13 +0200

    beautiful svg avatar

--- components/mod-card.tsx
+++ components/mod-card.tsx
@@ -9,7 +9,7 @@ import { LinkListItem } from './link-list-item';
 import { Card } from './card';
 
 export const ModCard = (props: Mod) => {
-  const imagePath = `/mods/${props.gameKey}.jpg`;
+  const imagePath = `/mods/${props.gameKey}.png`;
 
   return (
     <Card>
