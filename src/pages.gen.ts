cc0d184c16750d418e86887cb805e038fa3988ee
Raicuparta <Raicuparta@users.noreply.github.com>
2025-11-27 10:21:40 +0100

    update deps

--- src/pages.gen.ts
+++ src/pages.gen.ts
@@ -4,20 +4,20 @@
 import type { PathsForPages, GetConfigResponse } from 'waku/router';
 
 // prettier-ignore
-import type { getConfig as Mod_getConfig } from './pages/[mod]';
+import type { getConfig as File_Mod_getConfig } from './pages/[mod]';
 // prettier-ignore
-import type { getConfig as Root_getConfig } from './pages/_root';
+import type { getConfig as File_Root_getConfig } from './pages/_root';
 // prettier-ignore
-import type { getConfig as Index_getConfig } from './pages/index';
+import type { getConfig as File_Index_getConfig } from './pages/index';
 // prettier-ignore
-import type { getConfig as RaiPal_getConfig } from './pages/rai-pal';
+import type { getConfig as File_RaiPal_getConfig } from './pages/rai-pal';
 
 // prettier-ignore
 type Page =
-| ({ path: '/[mod]' } & GetConfigResponse<typeof Mod_getConfig>)
-| ({ path: '/_root' } & GetConfigResponse<typeof Root_getConfig>)
-| ({ path: '/' } & GetConfigResponse<typeof Index_getConfig>)
-| ({ path: '/rai-pal' } & GetConfigResponse<typeof RaiPal_getConfig>);
+| ({ path: '/[mod]' } & GetConfigResponse<typeof File_Mod_getConfig>)
+| ({ path: '/_root' } & GetConfigResponse<typeof File_Root_getConfig>)
+| ({ path: '/' } & GetConfigResponse<typeof File_Index_getConfig>)
+| ({ path: '/rai-pal' } & GetConfigResponse<typeof File_RaiPal_getConfig>);
 
 // prettier-ignore
 declare module 'waku/router' {
@@ -28,4 +28,3 @@ declare module 'waku/router' {
     pages: Page;
   }
 }
-  
--- waku.config.ts
+++ waku.config.ts
@@ -2,14 +2,12 @@ import svgr from "vite-plugin-svgr";
 import { defineConfig } from "waku/config";
 
 export default defineConfig({
-	unstable_viteConfigs: {
-		common: () => ({
-			plugins: [svgr()],
-			build: {
-				rollupOptions: {
-					external: ["sharp", "ico-to-png", "lodepng"],
-				},
+	vite: {
+		plugins: [svgr()],
+		build: {
+			rollupOptions: {
+				external: ["sharp", "ico-to-png", "lodepng"],
 			},
-		}),
+		},
 	},
 });
