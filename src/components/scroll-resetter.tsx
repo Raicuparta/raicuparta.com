5092ddb28556a809c0c362e9fbaa9c1f7799414a
Raicuparta <Raicuparta@users.noreply.github.com>
2025-11-27 10:31:32 +0100

    update comment so i can find the waku scroll issue easier

--- src/components/scroll-resetter.tsx
+++ src/components/scroll-resetter.tsx
@@ -7,6 +7,8 @@ import { useRouter } from "waku";
 // but it scrolls immediately on click, instead of after navigation.
 // That's a bit jarring since you see the page first scroll up and then
 // load the next page a bit later. So I have my own thing for that.
+// I tried to make a PR but it was bad: https://github.com/wakujs/waku/pull/1516
+// And somene else's issue: https://github.com/wakujs/waku/issues/1437
 export function ScrollResetter() {
 	const { path } = useRouter();
 	const previousPath = useRef(path);
